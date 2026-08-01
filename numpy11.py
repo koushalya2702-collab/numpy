66558fc875d4c109299fe177d9a3bce2d126559e
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-01 15:51:38 +0530

    add numpy11.py

---
+++ numpy11.py
@@ -0,0 +1,37 @@
+#copy and view
+import numpy as np
+arr=np.array([1,2,3,4,5])
+x=arr.copy()
+x[0]=42
+print(arr)
+print(x)
+
+#view
+import numpy as np
+arr=np.array([1,2,3,4,5])
+x=arr.view()
+arr[0]=45
+print(arr)
+print(x)
+
+
+#example
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+x = arr.view()
+x[0] = 31
+
+print(arr)
+print(x)
+
+#Check if Array Owns its Data
+import numpy as np
+
+arr=np.array([1,2,3,4,5])
+
+x=arr.copy()
+y=arr.view()
+
+print(x.base)
+print(y.base)
