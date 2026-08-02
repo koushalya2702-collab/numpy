e2e2b43e156a7f53c9f8ee6c52b33473465665c3
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-02 16:20:56 +0530

    add numpy12.py

---
+++ numpy12.py
@@ -0,0 +1,10 @@
+#NumPy Array Shape
+import numpy as np
+arr=np.array([[1,2,3,4],[5,6,7,8]])
+print(arr.shape)
+
+#Example
+import numpy as np
+arr=np.array([1,2,3,4],ndmin=5)
+print(arr)
+print(arr.shape)
