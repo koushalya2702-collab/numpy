335faabaae9b3a9dce305dbf774f5d5c8ce8de1c
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-02 16:22:00 +0530

    add numpy14.py

---
+++ numpy14.py
@@ -0,0 +1,72 @@
+#NumPy Array Iterating
+import numpy as np
+arr=np.array([1,2,3,4])
+for x in arr:
+    print(x)
+
+
+#Iterating 2-D Arrays
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+  print(x)
+
+#Example
+import numpy as np
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+for x in arr:
+  for y in x:
+    print(y)
+
+
+#Iterating 3-D Arrays
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+for x in arr:
+  print(x)
+
+#Example
+import numpy as np
+
+arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+for x in arr:
+  for y in x:
+    for z in y:
+      print(z)
+
+#Iterating Arrays Using nditer()
+import numpy as np
+
+arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+
+for x in np.nditer(arr):
+  print(x)
+
+#Iterating Array With Different Data Types
+import numpy as np
+
+arr = np.array([1,2,3])
+for x in np.nditer(arr,flags=['buffered'],op_dtypes=['S']):
+  print(x)
+
+
+#Enumerated Iteration Using ndenumerate()
+import numpy as np
+arr=np.array([1,2,3])
+for idx,x in np.ndenumerate(arr):
+  print(idx,x)
+
+
+#Example
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8]])
+for idx,x in np.ndenumerate(arr):
+  print(idx,x)
