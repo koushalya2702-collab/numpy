becc2088962560d17a4ffcc00be5b66bd6a4cd48
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-05 23:13:02 +0530

    add numpy18.py

---
+++ numpy18.py
@@ -0,0 +1,27 @@
+#NumPy Sorting Arrays
+import numpy as np
+
+arr = np.array([3, 2, 0, 1])
+print(np.sort(arr))
+
+#example
+import numpy as np
+
+arr = np.array(['banana', 'cherry', 'apple'])
+
+print(np.sort(arr))
+
+
+#example
+import numpy as np
+
+arr = np.array([True, False, True])
+
+print(np.sort(arr))
+
+#Sorting a 2-D Array
+import numpy as np
+
+arr = np.array([[3, 2, 4], [5, 0, 1]])
+
+print(np.sort(arr))
