65e25a98746dbe2f71a924b312751f2a4387f8f3
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-06 21:51:09 +0530

    add numpy22.py

---
+++ numpy22.py
@@ -0,0 +1,39 @@
+#NumPy Products
+import numpy as np
+
+arr = np.array([1, 2, 3, 4])
+
+x = np.prod(arr)
+
+print(x)
+
+
+#example
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+x = np.prod([arr1, arr2])
+
+print(x)
+
+
+#Product Over an Axis
+import numpy as np
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([5, 6, 7, 8])
+
+newarr = np.prod([arr1, arr2], axis=1)
+
+print(newarr)
+
+#Cummulative Product
+import numpy as np
+
+arr = np.array([5, 6, 7, 8])
+
+newarr = np.cumprod(arr)
+
+print(newarr)
