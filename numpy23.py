0f2b8071b254c381bd79ce8fa88f8e574b2df4bb
koushalya2702-collab <koushalya2702@gmail.com>
2026-08-06 21:51:40 +0530

    add numpy23.py

---
+++ numpy23.py
@@ -0,0 +1,18 @@
+#Differences
+import numpy as np
+
+arr = np.array([10, 15, 25, 5])
+
+newarr = np.diff(arr)
+
+print(newarr)
+
+
+#example
+import numpy as np
+
+arr = np.array([10, 15, 25, 5])
+
+newarr = np.diff(arr, n=2)
+
+print(newarr)
